7220fe6369f6d74e76d50969bc3d56764e2f2bf3
Eros Martinelli <erosmartinelli@MacBook-Pro-di-Eros.local>
2022-12-04 19:37:38 +0100

    Not satisfied with this

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -7,30 +7,32 @@ import Course.Core
 import Course.List
 import Course.Functor
 import qualified Data.Set as S
+import Course.ListZipper (fromList)
+
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
 -- on a Mac - run this with:
 -- > fastAnagrams "Tony" "/usr/share/dict/words"
-fastAnagrams ::
-  Chars
-  -> FilePath
-  -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
 
-newtype NoCaseString =
-  NoCaseString
-    Chars
 
-ncString ::
-  NoCaseString
-  -> Chars
-ncString (NoCaseString s) =
-  s
+
+anag :: Chars -> List Chars -> List Chars
+anag name dictionary = listh . S.toList $
+  S.intersection (S.fromList . hlist . permutations $ name) (S.fromList . hlist $ dictionary)
+
+
+fastAnagrams :: Chars -> FilePath -> IO (List Chars)
+fastAnagrams name file = anag name . lines <$> readFile file
+
+newtype NoCaseString = NoCaseString Chars
+
+ncString :: NoCaseString  -> Chars
+ncString (NoCaseString s) = s
 
 instance Eq NoCaseString where
   (==) = (==) `on` map toLower . ncString
+
 
 instance Show NoCaseString where
   show = show . ncString

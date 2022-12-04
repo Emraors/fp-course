c1f5eab0e7c866df17bc6df779faae76d6f6ac01
Eros Martinelli <erosmartinelli@MacBook-Pro-di-Eros.local>
2022-12-04 19:37:01 +0100

    Finish Anagrams

--- src/Course/Anagrams.hs
+++ src/Course/Anagrams.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 
 module Course.Anagrams where
 
 import Course.Core
 import Course.List
 import Course.Functor
+import Distribution.TestSuite (TestInstance(name))
 
 {-
 
@@ -24,21 +27,16 @@ Functions that might help
 * on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
 
 -}
+-- intersectBy e xs ys =
+--  filter (\x -> any (e x) ys) xs
+
 
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-anagrams ::
-  Chars
-  -> FilePath
-  -> IO (List Chars)
-anagrams =
-  error "todo: Course.Anagrams#anagrams"
+anagrams :: Chars -> FilePath -> IO (List Chars)
+anagrams name dictionary = intersectBy equalIgnoringCase (permutations name) . lines <$> (readFile dictionary)
 
 -- Compare two strings for equality, ignoring case
-equalIgnoringCase ::
-  Chars
-  -> Chars
-  -> Bool
-equalIgnoringCase =
-  error "todo: Course.Anagrams#equalIgnoringCase"
+equalIgnoringCase :: Chars -> Chars -> Bool
+equalIgnoringCase = on (==) (map toLower)

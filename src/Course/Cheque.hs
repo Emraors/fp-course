684dc86f302283818c516a3a4101e0706f4e7237
Eros Martinelli <erosmartinelli@MacBook-Pro-di-Eros.local>
2022-12-07 00:51:40 +0100

    Finish Cheque but not satisfied with the solution

--- src/Course/Cheque.hs
+++ src/Course/Cheque.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Used otherwise as a pattern" #-}
 
 {-
 
@@ -26,12 +30,13 @@ import Course.Functor
 import Course.Applicative
 import Course.Monad
 
+
+
 -- $setup
 -- >>> :set -XOverloadedStrings
 
 -- The representation of the grouping of each exponent of one thousand. ["thousand", "million", ...]
-illion ::
-  List Chars
+illion :: List Chars
 illion =
   let preillion ::
         List (Chars -> Chars)
@@ -187,43 +192,29 @@ data Digit =
   | Seven
   | Eight
   | Nine
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
 
-showDigit ::
-  Digit
-  -> Chars
-showDigit Zero =
-  "zero"
-showDigit One =
-  "one"
-showDigit Two =
-  "two"
-showDigit Three =
-  "three"
-showDigit Four =
-  "four"
-showDigit Five =
-  "five"
-showDigit Six =
-  "six"
-showDigit Seven =
-  "seven"
-showDigit Eight =
-  "eight"
-showDigit Nine =
-  "nine"
+showDigit :: Digit -> Chars
+showDigit Zero = "zero"
+showDigit One = "one"
+showDigit Two = "two"
+showDigit Three = "three"
+showDigit Four = "four"
+showDigit Five = "five"
+showDigit Six = "six"
+showDigit Seven = "seven"
+showDigit Eight = "eight"
+showDigit Nine = "nine"
 
 -- A data type representing one, two or three digits, which may be useful for grouping.
 data Digit3 =
   D1 Digit
   | D2 Digit Digit
   | D3 Digit Digit Digit
-  deriving Eq
+  deriving (Eq, Show)
 
 -- Possibly convert a character to a digit.
-fromChar ::
-  Char
-  -> Optional Digit
+fromChar :: Char -> Optional Digit
 fromChar '0' =
   Full Zero
 fromChar '1' =
@@ -320,8 +311,103 @@ fromChar _ =
 --
 -- >>> dollars "456789123456789012345678901234567890123456789012345678901234567890.12"
 -- "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents"
-dollars ::
-  Chars
-  -> Chars
-dollars =
-  error "todo: Course.Cheque#dollars"
+
+
+--- NOTE TO MYSELF: The solution I found was smooth until I realized I had to deal with plurals
+
+
+partition :: Int -> List a -> List (List a)
+partition _ Nil = Nil
+partition n xs = take n xs :. partition n (drop n xs)
+
+toDigit3 :: List Digit -> Digit3
+toDigit3 (digit:.Nil) = D1 digit
+toDigit3 (digit1:.digit2:.Nil) = D2 digit2 digit1
+toDigit3 (digit1:.digit2:.digit32:.Nil) = D3 digit32 digit2 digit1
+
+
+showHundred :: Digit3 -> Chars
+showHundred (D3 digitHundred Zero Zero) = showDigit digitHundred ++ " hundred"
+showHundred (D3 Zero digitTen digit) = showTen (D2 digitTen digit)
+showHundred (D3 digitHundred digitTen digit) = showDigit digitHundred ++ " hundred and " ++ showTen (D2 digitTen digit)
+
+
+showTen :: Digit3 -> Chars
+showTen (D2 digitTen digit) = case digitTen of
+  Zero -> showDigit digit
+  Two -> showTenCase digit "twenty"
+  Three -> showTenCase digit "thirty"
+  Four -> showTenCase digit "forty"
+  Five -> showTenCase digit "fifty"
+  Six ->  showTenCase digit "sixty"
+  Seven -> showTenCase digit "seventy"
+  Eight -> showTenCase digit "eighty"
+  Nine -> showTenCase digit "ninety"
+  One -> case digit of
+    Zero -> "ten"
+    One -> "eleven"
+    Two -> "twelve"
+    Three -> "thirteen"
+    _ -> showDigit digit ++ "teen"
+  where showTenCase digit multiple = if digit == Zero
+          then multiple
+          else multiple ++ "-" ++ showDigit digit
+
+showDigit3 :: Digit3 -> Chars
+showDigit3 (D1 digit) = showDigit digit
+showDigit3 (D3 Zero Zero Zero) = ""
+showDigit3 (D3 digitHundred digitTen digit) = showHundred (D3 digitHundred digitTen digit)
+showDigit3 (D2 digitTen digit) = showTen (D2 digitTen digit)
+
+
+showDecimal :: Digit3 -> Chars
+showDecimal (D1 digit) = showDigit3 (D2 digit Zero)
+showDecimal digit = showDigit3 digit
+
+parseInput :: Chars -> Optional (List Digit)
+parseInput = sequence . filter (/= Empty) . map fromChar
+
+toIntegral :: Chars -> Optional (List Char)
+toIntegral integral = flatMap (\pair -> case pair of
+                            ("", b) -> b
+                            (_, "") -> ""
+                            (f, s) ->  s ++ " " ++ f ++ " ") . reverse
+                      <$> lift2 zip (pure illion)
+                        (map (showDigit3 . toDigit3) . partition 3 . reverse
+                      <$> parseInput integral)
+
+toDecimal :: Chars -> Optional Chars
+toDecimal decimal = showDecimal . toDigit3 . reverse . take 2 <$> parseInput decimal
+
+takeIntegral :: Chars -> Chars
+takeIntegral = takeWhile (/= '.')
+
+takeDecimal :: Chars -> Chars
+takeDecimal =  drop 1 . dropWhile (/= '.')
+
+splitDollars :: Chars -> (Chars, Chars)
+splitDollars input = (takeIntegral input, takeDecimal input)
+
+dollarsOptional :: Chars -> Optional Chars
+dollarsOptional input = case splitDollars input of
+  ("", decimal) ->  (\c -> if c == "one"
+                      then "zero dollars and " ++ c ++ " cent"
+                      else "zero dollars and " ++ c ++ " cents") <$> toDecimal decimal
+  (integral, "") -> (\c -> if c == "one" then c ++ " dollar and zero cents"
+                                         else c ++ " dollars and zero cents") <$> toIntegral integral
+  (integral, decimal) -> lift2 pluralHanlder
+                           (toIntegral integral)
+                           (toDecimal decimal)
+
+pluralHanlder :: Chars -> Chars -> Chars
+pluralHanlder p d = case (p,d) of
+            ("one", "one") ->  "one dollar and one cent"
+            (p, "one") ->  p ++ " dollars and one cent"
+            ("one", d) -> "one dollar and " ++ d ++ " cents"
+            otherwise ->  p ++ " dollars and " ++ d ++ " cents"
+
+dollars :: Chars -> Chars
+dollars input = case dollarsOptional input of
+  Full li -> li
+  Empty -> "something went wrong"
+
